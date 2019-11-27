38a9ee14b1ce4269b95ed222333eb4b983380f2c
jisoo <jjns97@mju.ac.kr>
2019-11-28 01:03:33 +0900

    customer_modify

---
+++ src/main/java/com/test/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.test.dao;
+
+import java.util.HashMap;
+
+import com.test.dto.CustomerDTO;
+
+public interface CustomerDAO {
+	
+	abstract public CustomerDTO listThisCustomer(String customer_Id, String customer_Password);
+	
+	abstract public int insertTheCustomer(HashMap<String, Object> cmap);
+	
+	abstract public int deleteTheCustomer(int customer_Index) ;
+
+	abstract public int update(CustomerDTO cust);
+	
+}
